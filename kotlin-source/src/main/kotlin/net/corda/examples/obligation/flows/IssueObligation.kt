1a5da5ac6c8258e5175215a50928ee63261c6f17
nitrek <koolestnitesh@gmail.com>
2018-06-03 00:34:34 +0530

    sigining change bug fix

--- kotlin-source/src/main/kotlin/net/corda/examples/obligation/flows/IssueObligation.kt
+++ kotlin-source/src/main/kotlin/net/corda/examples/obligation/flows/IssueObligation.kt
@@ -1,6 +1,7 @@
 package net.corda.examples.obligation.flows
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.confidential.IdentitySyncFlow
 import net.corda.confidential.SwapIdentitiesFlow
 import net.corda.core.contracts.Amount
 import net.corda.core.flows.*
@@ -8,6 +9,7 @@ import net.corda.core.identity.Party
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 import net.corda.core.utilities.ProgressTracker
+import net.corda.core.contracts.Command
 import net.corda.core.utilities.ProgressTracker.Step
 import net.corda.core.utilities.seconds
 import net.corda.examples.obligation.Obligation
@@ -29,6 +31,9 @@ object IssueObligation {
             object INITIALISING : Step("Performing initial steps.")
             object BUILDING : Step("Building and verifying transaction.")
             object SIGNING : Step("Signing transaction.")
+            object SYNCING : ProgressTracker.Step("Syncing identities.") {
+                override fun childProgressTracker() = IdentitySyncFlow.Send.tracker()
+            }
             object COLLECTING : Step("Collecting counterparty signature.") {
                 override fun childProgressTracker() = CollectSignaturesFlow.tracker()
             }
@@ -71,10 +76,10 @@ object IssueObligation {
             progressTracker.currentStep = COLLECTING
             val lenderFlow = initiateFlow(lender)
             val stx = subFlow(CollectSignaturesFlow(
-                    ptx,
+                   partiallySignedTx = ptx,
                     sessionsToCollectFrom = sessions,
-                    listOf(ourSigningKey),
-                    COLLECTING.childProgressTracker())
+                    myOptionalKeys = listOf(ourSigningKey),
+                    progressTracker = COLLECTING.childProgressTracker())
             )
 
             // Step 5. Finalise the transaction.
