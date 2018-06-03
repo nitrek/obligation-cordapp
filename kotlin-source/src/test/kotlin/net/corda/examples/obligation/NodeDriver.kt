d2807c5b6149053e2844b2f5df7ed3c27f9a5930
nitrek <koolestnitesh@gmail.com>
2018-06-03 11:40:11 +0530

    added observer

--- kotlin-source/src/test/kotlin/net/corda/examples/obligation/NodeDriver.kt
+++ kotlin-source/src/test/kotlin/net/corda/examples/obligation/NodeDriver.kt
@@ -10,10 +10,12 @@ fun main(args: Array<String>) {
 
     driver(isDebug = true, startNodesInProcess = true, waitForAllNodesToFinish = true) {
         val (nodeA, nodeB, nodeC) = listOf(
-                startNode(providedName = CordaX500Name("PartyA", "London", "GB"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("PartyB", "New York", "US"), rpcUsers = listOf(user)),
-                startNode(providedName = CordaX500Name("PartyC", "Paris", "FR"), rpcUsers = listOf(user))
-        ).map { it.getOrThrow() }
+                startNode(providedName = CordaX500Name("Citibank", "London", "GB"), rpcUsers = listOf(user)),
+                startNode(providedName = CordaX500Name("Barclays", "New York", "US"), rpcUsers = listOf(user)),
+                startNode(providedName = CordaX500Name("HSBC", "Paris", "FR"), rpcUsers = listOf(user)),
+                startNode(providedName = CordaX500Name("Observer", "Paris", "FR"), rpcUsers = listOf(user))
+        ).
+        map { it.getOrThrow() }
 
         startWebserver(nodeA)
         startWebserver(nodeB)
